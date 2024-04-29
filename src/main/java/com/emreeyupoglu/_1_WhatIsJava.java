f34935432320d318e443b59a9d8f54ae8f7380be
emono469 <metro.1997@hotmail.com>
2024-04-29 12:06:05 +0300

    Java

--- src/main/java/com/emreeyupoglu/_1_WhatIsJava.java
+++ src/main/java/com/emreeyupoglu/_1_WhatIsJava.java
@@ -1,2 +1,11 @@
-package com.emreeyupoglu;public class _1_WhatIsJava {
+package com.emreeyupoglu;
+
+public class _1_WhatIsJava {
+
+    // single comment
+    /* multiple comment */
+
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Nasılsınız");
+    }
 }
